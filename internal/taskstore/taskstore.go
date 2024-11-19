5efc1a975aac8da1a5ff4235c872662838874827
Manas Mishra <manas@ManasMishra.>
2024-11-19 23:21:30 +0530

    added lot of functions

--- internal/taskstore/taskstore.go
+++ internal/taskstore/taskstore.go
@@ -1,7 +1,50 @@
 package taskstore
 
-type TaskStore struct{}
+type TaskStoreError string
+
+func (terr TaskStoreError) Error() string {
+	return string(terr)
+}
+
+const (
+	TaskNotFoundError TaskStoreError = "task not found"
+)
+
+type Task struct {
+	Id    uint64
+	Title string
+}
+
+var idCounter uint64 = 0
+
+type TaskStore struct {
+	tasks map[uint64]Task
+}
 
 func New() *TaskStore {
-	return &TaskStore{}
+	tasks := make(map[uint64]Task)
+	return &TaskStore{tasks}
+}
+
+func (ts *TaskStore) CreateTask(title string) uint64 {
+	idCounter++
+	task := Task{Id: idCounter, Title: title}
+	ts.tasks[idCounter] = task
+	return task.Id
+}
+
+func (ts *TaskStore) GetTask(id uint64) (Task, error) {
+	task, ok := ts.tasks[id]
+	if !ok {
+		return Task{}, TaskNotFoundError
+	}
+	return task, nil
+}
+
+func (ts *TaskStore) GetAllTask() []Task {
+	tasks := make([]Task, len(ts.tasks))
+	for _, task := range ts.tasks {
+		tasks = append(tasks, task)
+	}
+	return tasks
 }
--- internal/taskstore/taskstore_test.go
+++ internal/taskstore/taskstore_test.go
@@ -9,3 +9,93 @@ func TestNew(t *testing.T) {
 		t.Errorf("New() returned nil")
 	}
 }
+
+func TestCreateTask(t *testing.T) {
+
+	var ts *TaskStore = New()
+
+	var id uint64 = ts.CreateTask("Task 1")
+
+	if id == 0 {
+		t.Errorf("CreateTask() returned 0")
+	}
+
+	task, err := ts.GetTask(id)
+	if err != nil {
+		t.Errorf("GetTask() returned error: %v", err)
+	}
+
+	if task.Id != id {
+		t.Errorf("incorrect task id: %v", task.Id)
+	}
+
+	if task.Title != "Task 1" {
+		t.Errorf("incorrect task title: %v", task.Title)
+	}
+}
+
+func TestGetTask(t *testing.T) {
+	t.Run("add and get 1 task", func(t *testing.T) {
+		var ts *TaskStore = New()
+		ts.CreateTask("Task 1")
+
+		task, err := ts.GetTask(1)
+
+		if err != nil {
+			t.Errorf("GetTask() returned error: %v", err)
+		}
+
+		if task.Title != "Task 1" {
+			t.Errorf("incorrect task title: %v", task.Title)
+		}
+	})
+
+	t.Run("add and get 2 tasks", func(t *testing.T) {
+		var ts *TaskStore = New()
+		id1 := ts.CreateTask("Task 1")
+		id2 := ts.CreateTask("Task 2")
+
+		task1, err := ts.GetTask(id1)
+
+		if err != nil {
+			t.Errorf("GetTask() returned error: %v", err)
+		}
+
+		if task1.Id != id1 {
+			t.Errorf("incorrect task id: %v", task1.Id)
+		}
+
+		if task1.Title != "Task 1" {
+			t.Errorf("incorrect task title: %v", task1.Title)
+		}
+
+		task2, err := ts.GetTask(id2)
+
+		if err != nil {
+			t.Errorf("GetTask() returned error: %v", err)
+		}
+
+		if task2.Id != id2 {
+			t.Errorf("incorrect task id: %v", task2.Id)
+		}
+
+		if task2.Title != "Task 2" {
+			t.Errorf("incorrect task title: %v", task2.Title)
+		}
+	})
+}
+
+func TestGetAllTask(t *testing.T) {
+	var ts *TaskStore = New()
+	ts.CreateTask("Task 1")
+
+	var tasks []Task = ts.GetAllTask()
+
+	if len(tasks) != 1 {
+		t.Errorf("GetAllTask() returned %v tasks", len(tasks))
+	}
+
+	if tasks[0].Title != "Task 1" {
+		t.Errorf("incorrect task title: %v", tasks[0].Title)
+	}
+}
