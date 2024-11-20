3e5bf44313bf62ebc81e136c37b128ab4e122919
Manas Mishra <manas@ManasMishra.>
2024-11-21 02:48:13 +0530

    able to create tasks

--- taskserver/taskserver.go
+++ taskserver/taskserver.go
@@ -3,17 +3,25 @@ package taskserver
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/manasm11/tasklist/taskstore"
 )
 
 func NewTaskServer() http.Handler {
+	ts := taskstore.New()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "GET" {
+		switch r.Method {
+		case "GET":
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(ts.GetAllTask())
+		case "POST":
+			w.WriteHeader(http.StatusCreated)
+			var t taskstore.Task
+			json.NewDecoder(r.Body).Decode(&t)
+			ts.CreateTask(t.Title, nil, time.Time{})
+		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
 		}
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode([]taskstore.Task{})
 	})
 }
--- taskserver/taskserver_test.go
+++ taskserver/taskserver_test.go
@@ -1,6 +1,7 @@
 package taskserver
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -15,42 +16,83 @@ func Test_task(t *testing.T) {
 		h := httptest.NewServer(ts)
 		defer h.Close()
 
-		resp, err := http.Get(h.URL + "/task/")
+		resp := reqWithoutData(t, h, "GET", "/task/")
 
-		if err != nil {
-			t.Errorf("got an error %v", err)
-		}
-
-		if resp.StatusCode != 200 {
-			t.Errorf("expected 200 got %v", resp.StatusCode)
-		}
+		assertEqual(t, resp.StatusCode, 200)
 
 		tasks := []taskstore.Task{}
-		err = json.NewDecoder(resp.Body).Decode(&tasks)
+		err := json.NewDecoder(resp.Body).Decode(&tasks)
 
-		if err != nil {
-			t.Errorf("got an error %v", err)
-		}
+		assertEqual(t, err, nil)
+		assertEqual(t, len(tasks), 0)
 	})
 
 	t.Run("put at task without any data", func(t *testing.T) {
-		var ts http.Handler = NewTaskServer()
-		h := httptest.NewServer(ts)
+		h := httptest.NewServer(NewTaskServer())
 		defer h.Close()
 
-		req, err := http.NewRequest("PUT", h.URL+"/task/", nil)
-		if err != nil {
-			t.Errorf("got an error %v", err)
-		}
-		resp, err := http.DefaultClient.Do(req)
+		resp := reqWithoutData(t, h, "PUT", "/task/")
 
-		if err != nil {
-			t.Errorf("got an error %v", err)
-		}
-		if resp.StatusCode != http.StatusMethodNotAllowed {
-			t.Errorf("expected %v got %v", http.StatusMethodNotAllowed, resp.StatusCode)
-		}
+		assertEqual(t, resp.StatusCode, http.StatusMethodNotAllowed)
+	})
+
+	t.Run("post at task create task", func(t *testing.T) {
+		h := httptest.NewServer(NewTaskServer())
+		defer h.Close()
+
+		data := map[string]interface{}{"title": "Task 1"}
+		resp := reqWithJsonData(t, h, "POST", "/task/", data)
+
+		assertEqual(t, resp.StatusCode, http.StatusCreated)
+
+		resp = reqWithoutData(t, h, "GET", "/task/")
+
+		tasks := []taskstore.Task{}
+		json.NewDecoder(resp.Body).Decode(&tasks)
+
+		assertEqual(t, len(tasks), 1)
+		assertEqual(t, tasks[0].Title, "Task 1")
 	})
 }
 
+func assertEqual(t testing.TB, actual, expected interface{}) {
+	t.Helper()
+	if expected != actual {
+		t.Errorf("expected %v got %v", expected, actual)
+	}
+}
 
+func reqWithoutData(t testing.TB, h *httptest.Server, method, url string) *http.Response {
+	t.Helper()
+	req, err := http.NewRequest(method, h.URL+url, nil)
+	if err != nil {
+		t.Errorf("got an error %v", err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Errorf("got an error %v", err)
+	}
+	return resp
+}
+
+func reqWithJsonData(t testing.TB, h *httptest.Server, method, url string, data interface{}) *http.Response {
+	t.Helper()
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		t.Errorf("got an error %v", err)
+	}
+
+	req, err := http.NewRequest(method, h.URL+url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		t.Errorf("got an error %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Errorf("got an error %v", err)
+	}
+	return resp
+}
